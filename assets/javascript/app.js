751749bbab7a2382ab16aab2ae2bfe3879376b8d
Theron Halsey <theron.halsey@gmail.com>
2018-03-15 20:05:43 -0400

    functional html and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,30 @@
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAc_VxsFuLfoo7-iUrzyxBtElVFGjLgb1I",
-    authDomain: "employeedbproj.firebaseapp.com",
-    databaseURL: "https://employeedbproj.firebaseio.com",
-    projectId: "employeedbproj",
-    storageBucket: "",
-    messagingSenderId: "573587009241"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAc_VxsFuLfoo7-iUrzyxBtElVFGjLgb1I",
+  authDomain: "employeedbproj.firebaseapp.com",
+  databaseURL: "https://employeedbproj.firebaseio.com",
+  projectId: "employeedbproj",
+  storageBucket: "",
+  messagingSenderId: "573587009241"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 
 
-$("#submit").on("click", function() {
-	var name = $("#newName").val().trim()
-	var role = $("#newRole").val().trim()
-	var startDate = $("#newStartDate").val().trim()
-	var monthRate = $("#newRate").val().trim()
+$("#submit").on("click", function () {
+  event.preventDefault();
+  var name = $("#newName").val().trim()
+  var role = $("#newRole").val().trim()
+  var startDate = $("#newStartDate").val().trim()
+  var monthRate = $("#newRate").val().trim()
 
-	database.ref().push({
-		name: name,
-		role: role,
-		startDate: startDate,
-		monthRate: monthRate
-		})
+  database.ref().push({
+    name: name,
+    role: role,
+    startDate: startDate,
+    monthRate: monthRate
+  })
 
 })
